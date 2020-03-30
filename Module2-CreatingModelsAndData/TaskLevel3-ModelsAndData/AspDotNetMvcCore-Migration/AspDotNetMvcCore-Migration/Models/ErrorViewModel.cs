eb4dac2cf4ba8737a502b2a500d4e66b846b001a
itdev-lenovo <it1devops@gmail.com>
2020-03-30 17:09:25 +0530

    code added

---
+++ Module2-CreatingModelsAndData/TaskLevel3-ModelsAndData/AspDotNetMvcCore-Migration/AspDotNetMvcCore-Migration/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspDotNetMvcCore_Migration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
